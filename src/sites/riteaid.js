3eb611cb50556dd87deaf997cb65ce8ba008e280
Jeff <RebootJeff@users.noreply.github.com>
2021-02-14 21:03:13 -0800

    add some site placeholders

---
+++ src/sites/riteaid.js
@@ -0,0 +1,11 @@
+const URL = 'https://www.riteaid.com/pharmacy/apt-scheduler'
+
+const checkAvailability = async () => {
+  // TODO
+}
+
+export default {
+  name: 'Rite Aid',
+  checker: checkAvailability,
+  url: URL,
+}
---
+++ src/sites/walgreens.js
@@ -0,0 +1,12 @@
+// TODO
+const URL = 'https://www.walgreens.com/findcare/vaccination/covid-19/location-screening'
+
+const checkAvailability = async () => {
+  // TODO
+}
+
+export default {
+  name: 'Walgreens',
+  checker: checkAvailability,
+  url: URL,
+}
