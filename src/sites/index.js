211471f3d37fec1c159ea9024f5467824d5b5d89
RebootJeff <RebootJeff@users.noreply.github.com>
2021-02-15 17:39:57 -0800

    re-disable debugSite

--- src/sites/index.js
+++ src/sites/index.js
@@ -1,9 +1,9 @@
 import cvs from './cvs.js'
-import debugSite from './debugSite.js'
+// import debugSite from './debugSite.js'
 import walgreens from './walgreens.js'
 
 const siteCheckers = [
-  debugSite,
+  // debugSite,
   cvs,
   walgreens,
 ]
