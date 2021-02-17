ba5dd40f17fafe082d46db9b3c7ab9f4fea022c4
RebootJeff <RebootJeff@users.noreply.github.com>
2021-02-16 17:48:02 -0800

    redo CVS checker

--- src/sites/cvs.js
+++ src/sites/cvs.js
@@ -1,41 +1,26 @@
-import createPageEvaluator from '../../utils/createPageEvaluator.js'
+import { SEARCH } from '../../privateConfig.js'
 
 const NAME = 'CVS'
 const URL = 'https://www.cvs.com/immunizations/covid-19-vaccine'
+const AVAILABLE_STATUS = 'Available'
 
-// TODO: Consider replacing fetch-based approach with automated browsing (like the Walgreens checker)
-/*
- * Sends a GET request and parses the JSON response
- * returns Promise<Boolean> - appointment availability
-*/
-const checker = async () => {
-  const STATE = 'MD'
-  const FETCH_URL = `https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.${STATE}.json?vaccineinfo`
-  const FETCH_CONFIG = {
-    'credentials': 'include',
-    'headers': {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0',
-      'Accept': '*/*',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Pragma': 'no-cache',
-      'Cache-Control': 'no-cache'
-    },
-    'referrer': 'https://www.cvs.com/immunizations/covid-19-vaccine',
-    'method': 'GET',
-    'mode': 'cors'
-  }
-  
-  // `window` reference means this must be run within context of Puppeteer page
-  return await window.fetch(FETCH_URL, FETCH_CONFIG)
-    .then(res => res.json())
-    .then(json => {
-      const cities = json.responsePayloadData.data[STATE]
-      return cities.some(city => city.status !== 'Fully Booked')
-    })
+// Selectors
+const STATE_LINK = `a[data-modal='vaccineinfo-${SEARCH.state}']`
+const STATUS_SPANS = '.covid-updates span.status'
+
+const checker = async (page) => {
+  await page.goto(URL)
+  await page.click(STATE_LINK)
+
+  return await page.$$eval(
+    STATUS_SPANS,
+    (nodes, targetText) => nodes.map(n => n.innerText).some(t => t === targetText),
+    AVAILABLE_STATUS
+  )
 }
 
 export default {
   name: NAME,
-  checker: createPageEvaluator(URL, checker, NAME),
+  checker,
   url: URL,
 }
